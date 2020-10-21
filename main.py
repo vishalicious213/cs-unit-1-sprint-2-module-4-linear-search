d2572304c29979271b6459a84fa6c0b569c489bf
Vish Singh <vishalicious213@gmail.com>
2020-10-21 20:48:44 +0000

    added start, stop, step to for loop to start at one less than the length (last number in array), go until before -1 (so, the 0-index) and go backwards (step -1)

--- main.py
+++ main.py
@@ -4,10 +4,15 @@ Rewrite the implementation of linear search below so that the algorithm searches
 """
 def linear_search(arr, target):
     # loop through each item in the input array
-    for idx in range(len(arr)):
+    for idx in range(len(arr)-1, -1, -1):
+        # print(arr[idx])
         # check if the item at the current index is equal to the target
         if arr[idx] == target:
             # return the current index as the match
             return idx
     # if we were able to loop through the entire array, the target is not present
     return -1
+
+
+test_arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+print(linear_search(test_arr, 13))
