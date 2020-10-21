f3d9a5ccd2ba23680c2d1ffd8e3186fe37b9aeef
Vish Singh <vishalicious213@gmail.com>
2020-10-21 20:42:19 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+
+"""
+Rewrite the implementation of linear search below so that the algorithm searches from the end of the list to the beginning.
+"""
+def linear_search(arr, target):
+    # loop through each item in the input array
+    for idx in range(len(arr)):
+        # check if the item at the current index is equal to the target
+        if arr[idx] == target:
+            # return the current index as the match
+            return idx
+    # if we were able to loop through the entire array, the target is not present
+    return -1
